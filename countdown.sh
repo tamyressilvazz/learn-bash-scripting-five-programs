c23c04a3a70975357fefcc1339887518d697894a
Tamyres silvazz <tamyressilvazz@outlook.com>
2025-05-18 18:56:32 +0000

    Save progress: 620.1

--- countdown.sh
+++ countdown.sh
@@ -2,7 +2,11 @@
 #Program that counts down to zero from a given argument
 if [[ $1 -gt 0 ]]
 then
-  echo true
+  for (( i = $1; i > 0; i-- ))
+  do
+    echo $i
+  done
 else
   echo Include a positive integer as the first argument.
 fi
+
