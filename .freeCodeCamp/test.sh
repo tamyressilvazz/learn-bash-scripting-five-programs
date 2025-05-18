5492d76d00478e8bd3f4b5ba7bebfb31ff1bf1b2
Tamyres silvazz <tamyressilvazz@outlook.com>
2025-05-18 20:44:57 +0000

    Save progress: 940.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -2,5 +2,8 @@
 #Bingo Number Generator
 echo -e "\n~~ Bingo Number Generator ~~\n"
 
-NUMBER=5
+NUMBER=$(( RANDOM % 75 + 1 ))
 echo $NUMBER
+
+TEXT="The next number is, "
+echo $TEXT
--- bingo.sh
+++ bingo.sh
@@ -4,3 +4,5 @@ echo -e "\n~~ Bingo Number Generator ~~\n"
 
 NUMBER=$(( RANDOM % 75 + 1 ))
 echo $NUMBER
+
+TEXT="The next number is, "
