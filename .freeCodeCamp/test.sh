14dc855e719321e3fce75ddac4625df6d7286b52
Tamyres silvazz <tamyressilvazz@outlook.com>
2025-05-19 01:49:54 +0000

    Save progress: 1190.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -6,5 +6,10 @@ echo -e "\n~~ Fortune Teller ~~\n"
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 echo ${RESPONSES[5]}
 
-N=$(( RANDOM %1 ))
+N=$(( RANDOM % 6 ))
 echo ${RESPONSES[$N]}
+
+GET_FORTUNE(){
+  echo "Ask a yes or no question:"
+}
+GET_FORTUNE
